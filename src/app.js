7df9894a8393f842bfb2a51bda019d1f51c9d467
JulioChacon <jcchacon3000@gmail.com>
2022-12-06 13:34:05 -0700

    ENVIO ROUTES Y CONTROLLER AGREGADO2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import CLIENTERoutes from "./routes/CLIENTE.routes.js";
+import ENVIORoutes from "./routes/ENVIO.routes.js";
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use("/", indexRoutes);
 app.use("/api", employeesRoutes);
 app.use("/api", CLIENTERoutes);
+app.use("/api", ENVIORoutes);
+
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
