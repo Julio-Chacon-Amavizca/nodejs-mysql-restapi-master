816e95aecb4ba3cb02fb25f3eb90b8753f980ebd
JulioChacon <jcchacon3000@gmail.com>
2022-12-21 17:58:18 -0700

    AGREGA CONTROLADOR ORDEN create ORDEN

--- src/controllers/ORDEN.controller.js
+++ src/controllers/ORDEN.controller.js
@@ -48,7 +48,7 @@ export const createORDEN = async (req, res) => {
       "INSERT INTO ORDEN (idPedidos, IdProducto, nomProducto, precioUnitario, cantidad, monto, fecha) VALUES (?, ?, ?, ?, ?, ?, ?)",
       [ idPedidos,IdProducto, nomProducto, precioUnitario, cantidad, monto, fecha ]
     );
-    res.status(201).json({ id: rows.idPedidos,IdProducto, nomProducto, precioUnitario, cantidad, monto, fecha });
+    res.status(201).json({ id: rows.insertId, idPedidos,IdProducto, nomProducto, precioUnitario, cantidad, monto, fecha });
   } catch (error) {
     return res.status(500).json({ message: "Something goes wrong" });
   }
